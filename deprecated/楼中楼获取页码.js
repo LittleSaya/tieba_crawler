7c3df6517ebf1a978c1ee6641bba29d2f79e254b
littlesaya <littlesaya@outlook.com>
2019-06-22 19:42:18 +0800

    remove dir /deprecated

--- deprecated/楼中楼获取页码.js
+++
@@ -1,9 +0,0 @@
-
-  let currentPageNo = null;
-  let currentPageNoReg = /<span\s+class\s*=\s*"tP">\s*(\d+)\s*<\/span>/i;
-  if (currentPageNoReg.test(text)) {
-    currentPageNo = parseInt(RegExp.$1);
-  } else {
-    logger.info('validator/comment-list: fail to find current page no');
-    return null;
-  }
